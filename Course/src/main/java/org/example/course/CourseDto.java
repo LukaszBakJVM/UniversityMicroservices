075ee63840ade9bd48cda61846bfa7167ef7c0a9
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-04-15 20:31:33 +0200

    Course  sort

--- Course/src/main/java/org/example/course/CourseDto.java
+++ Course/src/main/java/org/example/course/CourseDto.java
@@ -2,5 +2,5 @@ package org.example.course;
 
 import java.util.List;
 
-public record CourseDto(String course, List<String> subjectId) {
+public record CourseDto(String course, List<String> subject) {
 }

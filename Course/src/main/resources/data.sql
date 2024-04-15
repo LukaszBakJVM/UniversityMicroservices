47ad4984639a3395a005efe4bb1c41347ee0a9f5
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-04-15 20:36:06 +0200

    Course  sort

--- Course/src/main/resources/data.sql
+++ Course/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into university_course(course)values ('s');
+insert into university_course(course)values ('Fiz-Chem');
 insert into university_course_subject_name (university_course_id,subject_name) values (1,'Chemia'),(1,'Fizyka');

a38538d07bb294cc7e65340b10751d7a70e254ce
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-04-27 14:13:48 +0200

    data.sql

--- Course/src/main/resources/data.sql
+++ Course/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into university_course(course)values ('Fiz-Chem');
-insert into university_course_subject_name (university_course_id,subject_name) values (1,'Chemia'),(1,'Fizyka');
+insert into university_course_subject_name (university_course_id,subject_name) values (1,'Chemia'),(1,'Fizyka'),(1,'Matematyka');
+insert into university_course(course)values ('Humanistyczny');
+insert into university_course_subject_name (university_course_id,subject_name) values (2,'Jezyk Polski'),(2,'Jezyka Angielski'),(2,'Matematyka');
+insert into university_course(course)values ('Informatyczny');
+insert into university_course_subject_name (university_course_id,subject_name) values (3,'Matematyka'),(3,'Informatyka'),(3,'Matematyka');
+
+

d9f5e15dd5e362767430eed97afffb823c7e9e69
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-05-11 21:39:49 +0200

    Wiremock pom

--- Student/src/test/java/org/example/student/StudentApplicationTests.java
+++ Student/src/test/java/org/example/student/StudentApplicationTests.java
@@ -1,13 +1,45 @@
 package org.example.student;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class StudentApplicationTests {
+    @LocalServerPort
+    private static int dynamicPort;
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance().options(wireMockConfig().port(dynamicPort)).build();
+    @Autowired
+    WebTestClient webTestClient;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("baseUrl", wireMockServer::baseUrl);
+
+
+    }
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void recordWiremock() throws InterruptedException {
+        System.out.println(wireMockServer.getPort());
+        while (true) {
+            Thread.sleep(4000);
+        }
+
+    }
 }

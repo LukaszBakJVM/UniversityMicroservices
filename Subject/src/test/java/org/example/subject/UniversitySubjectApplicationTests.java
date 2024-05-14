aeabaff4b3e4129d65033fe393114299115ebc55
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-05-14 19:50:56 +0200

    Test Subject

--- Subject/src/test/java/org/example/subject/UniversitySubjectApplicationTests.java
+++ Subject/src/test/java/org/example/subject/UniversitySubjectApplicationTests.java
@@ -1,13 +1,63 @@
 package org.example.subject;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MySQLContainer;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class UniversitySubjectApplicationTests {
+    private static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.37-bookworm").withInitScript("schema.sql");
+    @Autowired
+    WebTestClient webTestClient;
+    @Autowired
+    SubjectRepository subjectRepository;
+
+
+    @BeforeAll
+    static void beforeAll() {
+        mysql.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+    }
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
     @Test
-    void contextLoads() {
+    void save() {
+        String response = "{\"subject\":\"Matematyka\"}";
+
+
+
+        SubjectDto subjectdto = new SubjectDto("Matematyka");
+
+
+        webTestClient.post()
+                .uri("/subject/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(subjectdto)
+                .exchange()
+                .expectStatus().isOk().expectBody().json(response); //
+
     }
+
 
 }
---
+++ Subject/src/test/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE university_subject (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    subject VARCHAR(255) NOT NULL
+    );
